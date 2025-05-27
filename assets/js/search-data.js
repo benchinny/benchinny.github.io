2823dbc49b8dffbf5066a7f61ffefa0c84999807
benchinny <ben.chinny@gmail.com>
2025-05-27 04:08:51 +0000

    Deploying to gh-pages from @ benchinny/benchinny.github.io@05590e74d25010b6d17abd4e9683e561c61e9359 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Materials for courses",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
